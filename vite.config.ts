da9068ce5d54b3d5f3125c5cc7daafe1e798b4e7
CM Lubinski <cmc333333@gmail.com>
2025-09-21 18:48:58 +0000

    Add a 404 page.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        '404': './404.html',
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
